1779adef4b1689e094ad99e1d37d4bc744b106b8
Tester <wandayusuf.yusuf@gmail.com>
2021-01-19 16:56:16 +0700

    baru

---
+++ shelf/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Dua
+    {
+        public void prints()
+        {
+            Console.WriteLine("tes"); 
+        }
+    }
+    class Satu
+    {
+        Dua two=new Dua(); 
+
+        public void dotwo()
+        {
+            two.prints(); 
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Satu one = new Satu();
+            one.dotwo(); 
+            
+        }
+    }
+
+    
+}
